22f1d055ebd690425a5249c3594d7bd6faf6f066
Matias <matiasnabarro@gmail.com>
2021-11-11 23:15:18 -0300

    modificado puerto

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
 
-app.listen(process.ENV.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
